88cffa9638d4a022e68e8c512b8dfd1135620e1f
angoloisaac83 <angoloisaac83@gmail.com>
2025-04-23 15:09:24 +0100

    nn

--- src/components/getstarted.js
+++ src/components/getstarted.js
@@ -16,10 +16,10 @@ function GetStarted(){
     return(
         <>
             <section className="w-full h-fit flex flex-col py-[50px] items-center justify-center gap-[45px] border-b-[0.3px] border-[#0c0d0f]">
-                <h1 className="text-[40px] max-[500px]:text-[30px]">
-                    <i className="text-[#00cc33] font-bold max-[400px]:w-[200px]">Get Started</i> in Just 3 Steps
+                <h1 className="text-[40px] max-[500px]:text-[30px] max-[500px]:w-[200px]">
+                    <i className="text-[#00cc33] font-bold">Get Started</i> in Just 3 Steps
                 </h1>
-                <div className="w-full items-center justify-center max-[400px]:flex-col flex flex-row gap-[20px]">
+                <div className="w-full items-center justify-center max-[500px]:flex-col flex flex-row gap-[20px]">
                     <div class="flex flex-col items-center w-[30%] relative"><img src="https://autosnipe.ai/_app/immutable/assets/step1.udBCdLvM.svg" alt="Step 1" height="32" width="32" class="mb-2 md:h-14 md:w-14" /> <div class="text-xs text-grey1 md:text-xl text-center max-w-40">Connect a Solana Wallet</div></div>
                     <div class=" flex items-center w-[15%] -translate-x-1/2 justify-between"><div class="h-1 w-1 bg-grey2 rounded-full"></div> <div class="border-t border-grey2 border-dashed h-[1] w-full"></div> <div class="h-1 w-1 bg-grey2 rounded-full"></div></div>                
                     <div class="flex flex-col items-center w-[30%] relative"><img src="https://autosnipe.ai/_app/immutable/assets/step2.DfBDx9Ov.svg" alt="Step 2" height="32" width="32" class="mb-2 md:h-14 md:w-14" /> <div class="text-xs text-grey1 md:text-xl text-center max-w-40">Deposit SOL To Autosnipe Wallet</div></div>
