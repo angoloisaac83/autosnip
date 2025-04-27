167f06d4485f73a6bf04dc9cb2c6617988f1dea5
angoloisaac83 <angoloisaac83@gmail.com>
2025-04-27 08:35:16 +0100

    ssdsd

--- src/app/copytrade/page.js
+++ src/app/copytrade/page.js
@@ -15,11 +15,11 @@ function CopyTrade(){
   };
     return(
         <>
-            <section className="w-full h-fit text-white flex flex-col pt-[300px] py-[50px] items-center justify-center gap-[20px] border-b-[0.3px] border-[#0c0d0f]">
+            <section className="w-full max-[500px]:w-[400px] h-fit text-white flex flex-col max-[500px]:pt-[700px] pt-[300px] py-[50px] items-center justify-center gap-[20px] border-b-[0.3px] border-[#0c0d0f]">
                 <h1 className="text-[40px] max-[500px]:text-[30px] text-center max-[500px]:w-[200px]">
                     Copy Traders at <i className="text-[#00cc33] font-bold">Hyperspeed!</i> 
                 </h1>
-                <p className="text-[20px] text-[#808080] pb-[20px]">Mirror buy & sell orders from targeted traders on Autosnipe</p>
+                <p className="text-[20px] text-[#808080] w-[390px] text-center pb-[20px]">Mirror buy & sell orders from targeted traders on Autosnipe</p>
                 <div className="w-[714px] text-center items-center justify-center max-[500px]:flex-col flex flex-row gap-[20px]">
                     <div class="flex flex-col items-center w-[30%] relative"><img src="https://autosnipe.ai/_app/immutable/assets/step1.udBCdLvM.svg" alt="Step 1" height="32" width="32" class="mb-2 md:h-14 md:w-14" /> <div class="text-xs text-grey1 md:text-xl text-center max-w-40">Copy a target address from any platform</div></div>
                     <div class=" flex items-center w-[15%] -translate-x-1/2 justify-between"><div class="h-1 w-1 bg-grey2 rounded-full"></div> <div class="border-t border-grey2 border-dashed h-[1] w-full"></div> <div class="h-1 w-1 bg-grey2 rounded-full"></div></div>                
--- src/app/trending/page.js
+++ src/app/trending/page.js
@@ -5,7 +5,7 @@ const Trending = () =>{
 
     return(
         <>
-            <section className="text-white max-[500px]:w-full w-[100%] pt-[1270px] max-[500px]:pt-[1570px] max-[500px]:px-[10px]">
+            <section className="text-white max-[500px]:w-full w-[100%] pt-[1270px] max-[500px]:pt-[3270px] max-[500px]:px-[10px]">
             <GetStarted />
             <h1 className="text-[40px] font-bold text-center">New Trending Meme Coins</h1>
             <p className="text-[12px] text-center">Find the hottest meme coins trending right now!</p>
