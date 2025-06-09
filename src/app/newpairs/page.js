7c238b284b5aa8cfa7c5c20d8b82b016245d849e
angoloisaac83 <110981606+angoloisaac83@users.noreply.github.com>
2025-06-09 20:27:14 +0100

    Update page.js

--- src/app/newpairs/page.js
+++ src/app/newpairs/page.js
@@ -5,7 +5,7 @@ const Trending = () =>{
 
     return(
         <>
-            <section className="text-white w-[100%] max-[500px]:w-full pt-[1270px] max-[500px]:pt-[3260px] max-[500px]:px-[10px]">
+            <section className="text-white w-[100%] max-[500px]:w-full pt-[1270px] max-[500px]:pt-[1460px] max-[500px]:px-[10px]">
                 <GetStarted />
                 <h1 className="text-[40px] font-bold text-center">New Meme Coins</h1>
                 <p className="text-[12px] text-center">Find the hottest meme coins trending right now!</p>
